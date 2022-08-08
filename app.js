ca9f0dcb8bbcb68125cd5e6ed75132cee042a443
Itamarbitan <98110091+Itamarbitan@users.noreply.github.com>
2022-08-08 23:44:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+const XLSX = require('xlsx');
+const path = require('path');
+const POST = 3000;
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs')
+const staticWebsite = path.join(__dirname , './views')
+
+app.use(express.static(staticWebsite))
+
+
+bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+
+// functions
+
+// function that create a new sheet
+
+const exportUsersToExcel = (workBook , UserList, workSheetName, filePath) => {
+
+    const worksheet = XLSX.utils.json_to_sheet(UserList)
+    XLSX.utils.book_append_sheet(workBook, worksheet ,workSheetName , true)
+    XLSX.writeFile(workBook, path.resolve(filePath));
+    return true;
+
+};
+
+
+// function that append data to a existing sheet
+
+
+const addToExistingSheet = (UserList, filePath, dogsName) => {
+    const workBook = XLSX.readFile('users.xlsx');
+    const worksheet = workBook.Sheets[dogsName];
+    XLSX.utils.sheet_add_json(worksheet, UserList, {skipHeader:true , origin:-1});
+    XLSX.writeFile(workBook, path.resolve(filePath));
+
+}
+
+
+
+
+
+let UserList = [];
+
+
+const filePath = './users.xlsx';
+
+
+
+
+// requests
+
+app.post('' , (req , res) => {
+    res.render('index');
+    UserList = [{
+        'dog' : `${req.body.dog}`,
+        'handler' : `${req.body.handler}`,
+        'day' : `${req.body.day}`,
+        'weather' : `${req.body.weather}`,
+        'food' : `${req.body.food}`,
+        'hour' : `${req.body.hour}`,
+        'positive_on_positive' : `${req.body.positive_on_positive}`,
+        'negetive_on_positive' : `${req.body.negetive_on_positive}`,
+        'positive_on_negetive' : `${req.body.positive_on_negetive}`,
+        'negetive_on_negetive' : `${req.body.negetive_on_negetive}`
+    
+    }];
+    const workBook = XLSX.readFile('users.xlsx');
+    const workSheetName = `${req.body.dog}`;
+
+
+
+    // console.log(UserList);
+
+
+
+
+    if (workBook.Sheets[workSheetName]){
+        console.log('true!!!')
+        addToExistingSheet(UserList, filePath , workSheetName);
+
+    }
+    else {
+        exportUsersToExcel(workBook , UserList , workSheetName , filePath);
+
+    }
+});
+
+
+
+
+app.get('' , (req , res) => {
+    res.render('index')}
+);
+
+
+app.listen(POST , () => console.log('the app is listening to port 3000!!'));
